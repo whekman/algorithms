71c9e9810c1062901460ce95e0af531d12b48722
Willem Hekman <hekmanwillem@gmail.com>
2024-01-16 10:53:36 +0100

    added bst search, from array

--- data_structures/trees/binary_search_tree/binary_search_tree.c
+++ data_structures/trees/binary_search_tree/binary_search_tree.c
@@ -10,7 +10,7 @@ struct node {
 	struct node * right;
 };
 
-void insert_node(struct node **tree, struct node * z){
+void tree_insert(struct node **tree, struct node *z){
 	
 	// y stores the point of insertion
 	struct node * y = NULL;
@@ -38,7 +38,7 @@ void insert_node(struct node **tree, struct node * z){
 		printf("Tree is Empty..\n");
 
 		// z become the root of the tree
-
+		free(*tree);
 		*tree = z;
 
 		printf("New root of tree has key: %d\n", z->key);
@@ -66,34 +66,69 @@ struct node * node_create(int key){
 
 }
 
-// walks the sub-tree pointed to by x
-void inorder_walk(struct node * x){
-	if (x != NULL){
-		inorder_walk(x->left);
-		printf("%d -> ", x->key);
-		inorder_walk(x->right);
+void tree_from_array(struct node **tree, int keys[], int n){
+
+	for(int i = 0; i < n; i++){
+		struct node *N = node_create(keys[i]);
+		tree_insert(tree, N);
 	}
+
 }
+
+struct node * tree_search(struct node *x, int key){
+
+	if ((x == NULL) || (key == x->key)){
+		return x;
+	}
+	if (key < x->key){
+		return tree_search(x->left, key);
+	} else {
+		return tree_search(x->right, key);
+	}
+
+}
+
+// walks the sub-tree pointed to by x
+// prints out the values in ascending order
+// with indent you can set the indenting (defualt = 0)
+// to help visualize the tree height
+// (negative values remove the indenting)
+void inorder_walk(struct node *x, int indent){
+
+	if (x != NULL){
+		inorder_walk(x->left, indent + 1);
+		
+		for(int i = 0; i < indent; i++){
+			printf("\t");
+		}
+		printf("%d \n", x->key);
+
+		inorder_walk(x->right, indent + 1);
+	}
+
+}
+
+
 
 int main(void){
 
 	// points to the root
-	struct node * tree = NULL;
+	struct node *tree = NULL;
 
-	struct node * n1 = node_create(4);
-	struct node * n2 = node_create(5);
-	struct node * n3 = node_create(3);
+    int keys[] = {8,7,2,3,5,5};
+    int n = sizeof(keys) / sizeof(keys[0]);
 
-	insert_node(&tree, n1);
-	insert_node(&tree, n2);
-	insert_node(&tree, n3);
+    tree_from_array(&tree, keys, n);
 
-	printf("%d\n", tree == n1);
+	inorder_walk(tree,0);
 
-	inorder_walk(tree);
+	struct node *p = NULL;
+	p = tree_search(tree, 3);
 
-	printf("KEY\n");
-	printf("%d\n", tree->right->key);
+	if(p != NULL){
+		printf("%d", p->key);	
+	}
+	
 
 	return 0;
 }
